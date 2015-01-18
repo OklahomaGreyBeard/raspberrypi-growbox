f5ae8ff46862bfefaa7166105ecfb98608b4ff25
Kevin Webb <kwebb@cs.swarthmore.edu>
2015-01-17 19:50:04 -0500

    Untested, lots more implemented: input thread, settings, etc.

--- main.py
+++ main.py
@@ -31,15 +31,45 @@
 import argparse
 import atexit
 import datetime
+import readline
 import sys
-import time
+import threading
 
 import RPi.GPIO
 import serial
 
-# TODO:
-# fan speed
-# Second thread for input
+LIGHTS_PIN = 26
+
+def print_readline(text):
+    sys.stdout.write('\r' + ' ' * (len(readline.get_line_buffer()) + 2) + '\r')
+    print text
+    sys.stdout.write('> ' + readline.get_line_buffer())
+    sys.stdout.flush()
+
+class Settings(object):
+    def __init__(self, args):
+        self.interval = args.interval
+        self.serial = args.serial
+        self.target_temp = args.temperature
+        self.schedule_lights(args.lights)
+
+    def schedule_lights(self, hours):
+        if hours >= 24.0:
+            raise Exception('No more than 24 hours per day...')
+
+        hour_span = int(hours / 2.0)
+        minute_span = 60.0 * ((hours / 2.0) - hour_span)
+
+        noon = datetime.datetime(1,1,1,12)
+        delta = datetime.timedelta(hours=hour_span, minutes=minute_span)
+
+        self.lights_on = (noon - delta).time()
+        self.lights_off = (noon + delta).time()
+
+        print_readline('New light schedule:')
+        print_readline('  on: %s,  off: %s' % (self.lights_on.strftime('%H:%M'),
+                                               self.lights_off.strftime('%H:%M')))
+
 
 def cleanup(teensy):
     print 'Cleaning up...'
@@ -48,54 +78,91 @@ def cleanup(teensy):
 
 def parse_teensy(serial_string):
     cmd, value = serial_string.split(':')
-    value = float(value)
 
-    if cmd == 'TMP':
-        value = value * (9.0 / 5.0) + 32
+    name, value, suffix = {
+        'TMP': ('Temperature', float(value) * (9.0 / 5.0) + 32, 'F'),
+        'HUM': ('Humidity', float(value), '%',)
+        'PWM': ('Fan speed', float(value) * 100.0 / 255.0, '%')
+    }
 
-    return cmd, value
+    print_readline('%s: %.2f %s' % (name, value, suffix))
+    return value
 
-def main_loop(args, ontime, offtime):
-    RPi.GPIO.setmode(RPi.GPIO.BCM)
-    RPi.GPIO.setup(26, RPi.GPIO.OUT)
-
-    teensy = serial.Serial(args.serial, 9600, timeout=1)
-    atexit.register(cleanup, teensy)
-
-    RPi.GPIO.output(26, 0)
-    teensy.write('PWM0.')
-    print parse_teensy(teensy.readline())
+def periodic_loop(settings, lock, wakeup):
+    lock.acquire()
 
     while True:
         teensy.write('TMP.')
-        print parse_teensy(teensy.readline())
+        temp = parse_teensy(teensy.readline())
+        # TODO: Adjust fan speed depending on current temperature.
 
         now = datetime.datetime.now().time()
-        if now > ontime and now < offtime:
-            print now, 'Lights on'
-            RPi.GPIO.output(26, 1)
+        if now > settings.lights_on and now < settings.lights_off:
+            RPi.GPIO.output(LIGHTS_PIN, 1)
         else:
-            print now, 'Lights off'
+            RPi.GPIO.output(LIGHTS_PIN, 0)
 
-        time.sleep(args.interval)
+        wakeup.wait(settings.interval)
 
-def compute_schedule(hours):
-    if hours >= 24.0:
-        print 'No more than 24 hours per day...'
-        sys.exit(1)
+def handle_fans(settings, args, wakeup):
+    percentage = float(args.split()[0])
+    if percentage < 0:
+        percentage = 0
+    if percentage > 100:
+        percentage = 100
 
-    hour_span = int(hours / 2.0)
-    minute_span = 60.0 * ((hours / 2.0) - hour_span)
+    pwm = int(percentage * 255.0 / 100.0)
 
-    noon = datetime.datetime(1,1,1,12)
-    delta = datetime.timedelta(hours=hour_span, minutes=minute_span)
+    teensy.write('PWM%d.' % pwm)
+    parse_teensy(teensy.readline())
 
-    ontime = (noon - delta).time()
-    offtime = (noon + delta).time()
+def handle_hum(settings, args, wakeup):
+    teensy.write('HUM.')
+    parse_teensy(teensy.readline())
 
-    print 'Light schedule:', ontime, offtime
+def handle_interval(settings, args, wakeup):
+    settings.interval = float(args.split()[0])
+    wakeup.notify()
 
-    return ontime, offtime
+def handle_hours(settings, args, wakeup):
+    settings.schedule_lights(float(args.split()[0]))
+    wakeup.notify()
+
+def handle_target(settings, args, wakeup):
+    settings.target_temp = float(args.split()[0])
+    wakeup.notify()
+
+def handle_temp(settings, args, wakeup):
+    teensy.write('TMP.')
+    parse_teensy(teensy.readline())
+
+def handle_wakeup(settings, args, wakeup):
+    wakeup.notify()
+
+handlers = {
+    'fans':        handle_fans,
+    'hours':       handle_hours,
+    'hum':         handle_hum,
+    'humidity':    handle_hum,
+    'interval':    handle_interval,
+    'target':      handle_target,
+    'target_temp': handle_target,
+    'temp':        handle_temp,
+    'temperature': handle_temp,
+    'wakeup':      handle_wakeup
+}
+
+def rep_loop(settings, lock, wakeup):
+    while True:
+        cmd, args = raw_input('> ').split(None, 1)
+
+        if cmd not in handlers:
+            print 'No such command: %s' % cmd
+            continue
+
+        lock.acquire()
+        handlers[cmd.lower()](settings, args, wakeup)
+        lock.release()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -109,6 +176,28 @@ if __name__ == '__main__':
             help='The target temperature, in degrees Fahrenheit. (float) [75]')
     args = parser.parse_args()
 
-    ontime, offtime = compute_schedule(args.lights)
+    # Create initial settings based on command line arguments.
+    settings = Settings(args)
 
-    main_loop(args, ontime, offtime)
+    # Initialize the GPIO pin that controls the lights.
+    RPi.GPIO.setmode(RPi.GPIO.BCM)
+    RPi.GPIO.setup(LIGHTS_PIN, RPi.GPIO.OUT)
+    RPi.GPIO.output(LIGHTS_PIN, 0)
+
+    # Initialize serial communication with the Teensy.
+    teensy = serial.Serial(settings.serial, 9600, timeout=1)
+    teensy.write('PWM0.')
+    parse_teensy(teensy.readline())
+
+    # Register a function to clean up on exit.
+    atexit.register(cleanup, teensy)
+
+    # Create a thread for user input / control.
+    lock = threading.Lock()
+    wakeup = threading.Condition(lock)
+    t = threading.Thread(target=rep_loop, args=(settings, lock, wakeup))
+    t.daemon = True
+    t.start()
+
+    # Drop into a permanent periodic loop.
+    periodic_loop(settings, lock, wakeup)
